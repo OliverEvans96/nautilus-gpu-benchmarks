b03306741ef69b683a12264834850293a8eeaaa7
Oliver Evans <oliverevans96@gmail.com>
2023-08-30 17:10:04 -0700

    Add download.py

---
+++ zihao/download.py
@@ -0,0 +1,13 @@
+import deep_stpp.main
+import glow.main
+import st_uq.main
+import tf_net.main
+
+def main():
+    deep_stpp.main.download()
+    glow.main.download()
+    st_uq.main.download()
+    tf_net.main.download()
+
+if __name__ == '__main__':
+    main()
