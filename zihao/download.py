02fc0766fe7cfab8cf184c555802a9012b8c1766
Oliver Evans <oliverevans96@gmail.com>
2023-08-30 17:13:08 -0700

    Use DATA_DIR in download.py

--- zihao/download.py
+++ zihao/download.py
@@ -1,9 +1,19 @@
+import os
+
 import deep_stpp.main
 import glow.main
 import st_uq.main
 import tf_net.main
 
 def main():
+    if 'DATA_DIR' in os.environ:
+        data_dir = os.environ['DATA_DIR']
+        print("Using data dir: {}".format(data_dir))
+        # Create the directory if it doesn't exist
+        os.makedirs(data_dir, exist_ok=True)
+        # Switch to that directory
+        os.chdir(data_dir)
+
     deep_stpp.main.download()
     glow.main.download()
     st_uq.main.download()
