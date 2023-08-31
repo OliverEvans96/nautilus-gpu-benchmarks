60a963e48cba1288a4ef018b5f78f3e6e5989e45
Oliver Evans <oliverevans96@gmail.com>
2023-08-31 12:45:21 -0700

    Clean up tf_net/main.py

--- zihao/tf_net/main.py
+++ zihao/tf_net/main.py
@@ -18,6 +18,8 @@ import torch.nn.functional as F
 from torch.utils import data
 from tqdm.auto import trange
 
+from s3fs.core import S3FileSystem
+
 from .model import LES
 from .penalty import DivergenceLoss
 from .train import Dataset, train_epoch, eval_epoch, test_epoch
@@ -43,24 +45,21 @@ model = LES(input_channels = input_length*2, output_channels = 2, kernel_size = 
             dropout_rate = dropout_rate, time_range = time_range).to(device)
 model = nn.DataParallel(model, device_ids=[0])
 
-from s3fs.core import S3FileSystem
-s3 = S3FileSystem(
-   key='V4870SVBWMMXDER34V7V',
-   secret='ArxQb8fpO9b9zgMoqIGcnCRCCAQOZR5GRkt4gr9G',
-   client_kwargs={
-      'endpoint_url': 'https://us-southeast-1.linodeobjects.com',
-      'region_name': 'US'
-   }
-)
-
 def download():
+    s3 = S3FileSystem(
+        key='V4870SVBWMMXDER34V7V',
+        secret='ArxQb8fpO9b9zgMoqIGcnCRCCAQOZR5GRkt4gr9G',
+        client_kwargs={
+            'endpoint_url': 'https://us-southeast-1.linodeobjects.com',
+            'region_name': 'US'
+        }
+    )
+
     if not exists('TF-net/rbc_data.pt'):
         s3.download('tfnet/rbc_data.pt', 'TF-net/rbc_data.pt')
 
 
 def data_gen():
-    if not exists('TF-net/rbc_data.pt'):
-        s3.download('tfnet/rbc_data.pt', 'TF-net/rbc_data.pt')
     data = torch.load('TF-net/rbc_data.pt')
 
     # standardization
@@ -82,37 +81,37 @@ def data_gen():
 
     return samples
 
+def main():
+    samples = data_gen()
+    train_set = Dataset(samples, train_indices, input_length + time_range - 1, 40, output_length, True)
+    valid_set = Dataset(samples, valid_indices, input_length + time_range - 1, 40, 6, True)
+    train_loader = data.DataLoader(train_set, batch_size = batch_size, shuffle = True, num_workers = 8)
+    valid_loader = data.DataLoader(valid_set, batch_size = batch_size, shuffle = False, num_workers = 8)
+    loss_fun = torch.nn.MSELoss()
+    regularizer = DivergenceLoss(torch.nn.MSELoss())
+    coef = 0
 
-samples = data_gen()
-train_set = Dataset(samples, train_indices, input_length + time_range - 1, 40, output_length, True)
-valid_set = Dataset(samples, valid_indices, input_length + time_range - 1, 40, 6, True)
-train_loader = data.DataLoader(train_set, batch_size = batch_size, shuffle = True, num_workers = 8)
-valid_loader = data.DataLoader(valid_set, batch_size = batch_size, shuffle = False, num_workers = 8)
-loss_fun = torch.nn.MSELoss()
-regularizer = DivergenceLoss(torch.nn.MSELoss())
-coef = 0
+    optimizer = torch.optim.Adam(model.parameters(), learning_rate, betas = (0.9, 0.999), weight_decay = 4e-4)
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size = 1, gamma = 0.9)
 
-optimizer = torch.optim.Adam(model.parameters(), learning_rate, betas = (0.9, 0.999), weight_decay = 4e-4)
-scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size = 1, gamma = 0.9)
-
-train_mse = []
-valid_mse = []
-test_mse = []
-for i in trange(5, file=sys.stdout):
-    start = time.time()
-    torch.cuda.empty_cache()
-    scheduler.step()
-    model.train()
-    train_mse.append(train_epoch(train_loader, model, optimizer, loss_fun, coef, regularizer))#
-    model.eval()
-    mse, preds, trues = eval_epoch(valid_loader, model, loss_fun)
-    valid_mse.append(mse)
-    if valid_mse[-1] < min_mse:
-        min_mse = valid_mse[-1] 
-        best_model = model 
-        # torch.save(best_model, "model.pth")
-    end = time.time()
-    if (len(train_mse) > 50 and np.mean(valid_mse[-5:]) >= np.mean(valid_mse[-10:-5])):
-            break
-    print(train_mse[-1], valid_mse[-1], round((end-start)/60,5))
-print(time_range, min_mse)
+    train_mse = []
+    valid_mse = []
+    test_mse = []
+    for i in trange(5, file=sys.stdout):
+        start = time.time()
+        torch.cuda.empty_cache()
+        scheduler.step()
+        model.train()
+        train_mse.append(train_epoch(train_loader, model, optimizer, loss_fun, coef, regularizer))#
+        model.eval()
+        mse, preds, trues = eval_epoch(valid_loader, model, loss_fun)
+        valid_mse.append(mse)
+        if valid_mse[-1] < min_mse:
+            min_mse = valid_mse[-1]
+            best_model = model
+            # torch.save(best_model, "model.pth")
+        end = time.time()
+        if (len(train_mse) > 50 and np.mean(valid_mse[-5:]) >= np.mean(valid_mse[-10:-5])):
+                break
+        print(train_mse[-1], valid_mse[-1], round((end-start)/60,5))
+    print(time_range, min_mse)
